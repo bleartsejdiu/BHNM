98d9e775f90224479b2968ffc888a18550246f5f
bleartsejdiu <69080739+bleartsejdiu@users.noreply.github.com>
2025-04-11 22:49:25 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,5 +10,45 @@ document.addEventListener('DOMContentLoaded', function () {
     closeBtn.addEventListener('click', () => {
         sidebarMenu.classList.remove('active');
     });
+
+    document.querySelectorAll('.product-card').forEach(card => {
+        card.addEventListener('click', function () {
+            const name = card.getAttribute('data-name');
+            const price = card.getAttribute('data-price');
+            const image = card.getAttribute('data-image');
+            const description = card.getAttribute('data-description');
+
+            document.getElementById('modalName').innerText = name;
+            document.getElementById('modalPrice').innerText = price;
+            document.getElementById('modalImage').src = image;
+            document.getElementById('modalDescription').innerText = description;
+
+            $('#productModal').modal('show');
+        });
+    });
 });
 
+function addToCart() {
+    window.location.href = 'index.html';
+}
+
+function performSearch() {
+    var query = $('#searchInput').val();
+    if (query !== "") {
+        window.location.href = '/search?q=' + encodeURIComponent(query);
+    }
+}
+
+$(document).ready(function() {
+    function toggleSearch() {
+        $('#searchOverlay').toggleClass('hidden');
+    }
+
+    $('.search-icon').on('click', toggleSearch);
+
+    $('#searchInput').keypress(function(e) {
+        if (e.which === 13) {
+            performSearch();
+        }
+    });
+});
